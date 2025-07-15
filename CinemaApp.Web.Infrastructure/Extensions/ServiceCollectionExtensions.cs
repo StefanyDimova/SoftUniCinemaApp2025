689afc679902f15015735441d91060b2f9a096dc
StefanyDimova <stefany.asenova@gmail.com>
2025-07-15 16:28:15 +0300

    Extension method for services and their interfaces.

---
+++ CinemaApp.Web.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CinemaApp.Web.Infrastructure.Extensions
+{
+
+    // метод , който намира сървиси и интерфейси и ги регистрира 
+    public static class ServiceCollectionExtensions
+    {
+        private static readonly string ServiceInterfacePrefix = "I";
+        private static readonly string ServiceTypeSuffix = "Service";
+        public static IServiceCollection AddUserDefinedServices(this IServiceCollection serviceCollection, Assembly serviceAssembly)
+        {
+            Type[] serviceClasses = serviceAssembly.GetTypes()
+                .Where(t => !t.IsInterface &&
+                    t.Name.EndsWith(ServiceTypeSuffix))
+                .ToArray();
+
+            foreach (Type serviceClass in serviceClasses)
+            {
+                Type[] serviceClassInterfaces = serviceClass.GetInterfaces();
+                if(serviceClassInterfaces.Length == 1 && serviceClassInterfaces.First().Name.EndsWith(ServiceTypeSuffix) && serviceClassInterfaces.First().Name.StartsWith(ServiceInterfacePrefix))
+                {
+                    Type serviceClassInterface = serviceClassInterfaces.First();
+
+                    serviceCollection.AddScoped(serviceClassInterface, serviceClass);
+                }
+
+            }
+
+            return serviceCollection;
+        }
+    }
+}
